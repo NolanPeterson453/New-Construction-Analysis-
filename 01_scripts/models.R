8bd0d8a04c5cee64ddb3c1cb656c6693177b18c1
NolanPeterson453 <79056951+NolanPeterson453@users.noreply.github.com>
2022-01-06 14:50:50 -0600

    Create models.R
    script containing code for the various models implemented

---
+++ 01_scripts/models.R
@@ -0,0 +1,108 @@
+################################################################################
+# Model building for New Const. Home Analysis                                  #
+# Nolan Peterson                                                               #
+# 12/28/2021                                                                   #
+# https://github.com/NolanPeterson453/New-Construction-Analysis-               #
+################################################################################
+
+# Library statments 
+library(tidyverse)
+library(splitstackshape)
+library(randomForest)
+library(leaps)
+
+# Read in data 
+soc_data <- read_csv(file = "https://raw.githubusercontent.com/NolanPeterson453/New-Construction-Analysis-/main/02_data/cleaned_soc.csv")
+
+# Assign column types 
+factor_vars <- c("ACS", "AGER", "ASSOC", "BASE", "CON", "DECK", "DET", 
+                 "DIV", "FNBS", "FOYER", "FRAME", "GAR", "HEAT", "HEAT2", 
+                 "LNDR", "MFGS", "PATI", "PRCH", "SEWER", "STOR", "WAL1", 
+                 "WAL2", "WALS","WATER", "BEDR", "FPLS", "FULB", "HAFB", "FUEL",
+                 "FUEL2")
+
+contin_vars <- c("FSLPR", "FSQFS", "LOTV", "FFNSQ", "AREA")
+
+soc_data <- soc_data %>% 
+  convert(fct(factor_vars),
+          num(contin_vars))
+
+# Expand data set by replicate weight 
+soc_data_expanded <- expandRows(soc_data, 'WEIGHT')
+
+# Create training and test splits 
+set.seed(42)
+sample <- sample(c(TRUE, FALSE), 
+                 nrow(soc_data), 
+                 replace=TRUE, 
+                 prob=c(0.8,0.2))
+train <- soc_data[sample, ]
+test <- soc_data[!sample, ]
+
+
+################################################################################
+# Ordinary Linear Regression 
+################################################################################
+
+## Testing assumptions of linear regression 
+
+# Correlation matrix of continous predictors and response
+  # No apparent multicollinearity 
+cor(select(train, contin_vars))
+
+# Null model 
+H_0_model <- lm(FSLPR ~ 1, data = train, weights = WEIGHT)
+summary(H_0_model)
+
+# Full model 
+H_a_model_1 <- lm(FSLPR ~ . - WEIGHT, data = train, weights = WEIGHT)
+summary(H_a_model_1) # Adjusted R-squared:  0.579
+
+# Analyzing plots of full model 
+  # potential issue with normality of residuals 
+  # potnetial high leverage points 
+plot(H_a_model_1)
+
+## Testing log transformations in order to correct 
+
+# Analyzing scatter plots of continous predictors and response 
+
+  # FSQFS without log transformation 
+plot(soc_data$FSQFS, soc_data$FSLPR)
+  # FSQFS with log transformation 
+plot(log(soc_data$FSQFS), soc_data$FSLPR)
+
+  # LOTV without log transformation 
+plot(soc_data$LOTV, soc_data$FSLPR)
+  # LOTV with log transformation 
+plot(log(soc_data$LOTV), soc_data$FSLPR)
+
+  # AREA without log transformation 
+plot(soc_data$AREA, soc_data$FSLPR)
+  # AREA with log transformation 
+plot(log(soc_data$AREA), soc_data$FSLPR)
+
+# Fit model with log transformations  
+H_a_model_2 <- lm(FSLPR ~ . + log(FSQFS) + log(LOTV) + log(AREA) 
+                  - WEIGHT - FSQFS - LOTV - AREA, 
+                  data = train,
+                  weights = WEIGHT)
+summary(H_a_model_2) #  Adjusted R-squared:  0.5555
+
+# Analyzing plots of transformed model 
+  # potential issue with normality of residuals 
+  # potnetial high leverage points 
+plot(H_a_model_2)
+
+## Model Selction 
+
+# Choose best model using stepwise selction with AIC as criterion 
+n <- dim(soc_data)[1]
+H_a_model_3 <- step(H_a_model_1, direction = "both", k = log(n))
+summary(H_a_model_3) # Adjusted R-squared:  0.5754
+
+################################################################################
+# Random Forest Model 
+################################################################################
+
+random
