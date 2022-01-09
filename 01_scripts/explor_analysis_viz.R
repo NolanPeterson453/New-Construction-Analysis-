ea5aea4e0c4b6dfc8d7ba2beb215dbdce296e8c6
NolanPeterson453 <79056951+NolanPeterson453@users.noreply.github.com>
2022-01-08 23:22:04 -0600

    updated report

--- 01_scripts/explor_analysis_viz.R
+++ 01_scripts/explor_analysis_viz.R
@@ -10,6 +10,7 @@ library(tidyverse)
 library(hablar)
 library(scales)
 library(ggthemes)
+library(ggrepel)
 
 # Read in data 
 soc_data <- read_csv(file = "https://raw.githubusercontent.com/NolanPeterson453/New-Construction-Analysis-/main/02_data/cleaned_soc.csv")
@@ -40,3 +41,42 @@ soc_data %>%
        x = "Final Sale Price of Home", 
        y = "Count of Homes") +
   theme_economist() 
+
+# Examine correlation between response and key variables 
+econ_palette <- c("#e3120b", "#4a4a4a", "#91b8bd",
+                  "#acc8d4", "#9ae5de", "#244747",
+                  "#336666", "#8abbd0", "#660663")
+soc_data %>% 
+  group_by(DIV) %>% 
+  summarise(FSLPR = mean(FSLPR),
+            FSQFS = mean(FSQFS),
+            WEIGHT = sum(WEIGHT)) %>% 
+  ggplot(aes(x = FSQFS, y = FSLPR, 
+             color = DIV, 
+             size = WEIGHT, 
+             label = c("New England",
+                       "Middle Atlantic",
+                       "East North Central",
+                       "West North Central",
+                       "South Atlantic",
+                       "East South Central",
+                       "West South Central",
+                       "Mountain",
+                       "Pacific"))) + 
+  geom_point() + 
+  scale_y_continuous(labels = dollar_format()) +
+  labs(title = "Average Sale Price vs Average Area Grouped By Region",
+       x = "Square Foot Area of Home",
+       y = "Final Sale Price",
+       color = "Region",
+       size = "Sample Size") +
+  scale_color_manual(values = econ_palette, labels = c("New England",
+                                                   "Middle Atlantic",
+                                                   "East North Central",
+                                                   "West North Central",
+                                                   "South Atlantic",
+                                                   "East South Central",
+                                                   "West South Central",
+                                                   "Mountain",
+                                                   "Pacific")) + 
+  geom_text_repel(size = 3.88)
